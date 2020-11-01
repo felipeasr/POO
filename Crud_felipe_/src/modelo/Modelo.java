735704213ed3e053f7ee54e7bbe059eab3618961
felipe <54966505+felipeasr@users.noreply.github.com>
2020-11-01 02:18:07 -0300

    Add crud

---
+++ Crud_felipe_/src/modelo/Modelo.java
@@ -0,0 +1,36 @@
+package modelo;
+
+public class Modelo {
+
+	private String nome;
+	private String senha;
+	
+	public Modelo(String n, String s){
+		this.nome = n;
+		this.senha = s;
+	}
+	
+	public Modelo(){
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+}
+
